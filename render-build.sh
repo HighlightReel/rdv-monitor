70f4f3fbc2fa3b93e5ae83763073de3a4c2f7eed
Hugo Hubert <hugohubert@Mac.lan>
2025-10-16 20:58:41 +0200

    Render fix: install Chromium at build

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 pip install -r requirements.txt
-python3 -m playwright install chromium
+# install browsers into the project directory that gets deployed
+export PLAYWRIGHT_BROWSERS_PATH=0
+python -m playwright install chromium
